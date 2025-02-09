c0de87978106866a89ceb5fa8124df6eb95e143c
syedraza <majidraza@hotmail.com>
2025-02-08 22:39:07 -0500

    syed crewai

---
+++ syed-AgenticAI/learn_01/src/learn_01/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from crew import Learn01Crew
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.git Z
+    """
+ 
+
+    Learn01Crew().crew().kickoff()
+
+
+run()
